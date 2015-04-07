80656c4a600f530d0ade84af67348cedcecbd29b
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-04-07 12:19:35 -0400

    Video, booklet, common page styling

--- sites/all/themes/palliative_chemo/template.php
+++ sites/all/themes/palliative_chemo/template.php
@@ -4,5 +4,68 @@
  * porto_sub_form_alter(&$form, &$form_state, $form_id) { ... }
  */
 
+/**
+ * Overrides theme_item_list().
+ */
+function palliative_chemo_item_list($variables) {
+  $items = $variables['items'];
+  $title = $variables['title'];
+  $type = $variables['type'];
+  $variables['attributes']['class'] = 'pagination pagination-lg center display-table clearfix';
+  $attributes = $variables['attributes'];
+
+  // Only output the list container and title, if there are any list items.
+  // Check to see whether the block title exists before adding a header.
+  // Empty headers are not semantic and present accessibility challenges.
+  $output = '';
+  if (isset($title) && $title !== '') {
+    $output .= '<h3>' . $title . '</h3>';
+  }
+
+  if (!empty($items)) {
+    $output .= "<$type" . drupal_attributes($attributes) . '>';
+    $num_items = count($items);
+    $i = 0;
+    foreach ($items as $item) {
+      $attributes = array();
+      $children = array();
+      $data = '';
+      $i++;
+      
+      //if ( is_array($item) && in_array('pager-current', $item['class'])) {
+      if ( isset($item['class']) && is_array($item) && in_array('pager-current', $item['class'])) {
+        $item['class'] = array('active');
+        $item['data'] = '<a href="#">' . $item['data'] . '</a>';
+      }
+
+      if (is_array($item)) {
+        foreach ($item as $key => $value) {
+          if ($key == 'data') {
+            $data = $value;
+          }
+          elseif ($key == 'children') {
+            $children = $value;
+          }
+          else {
+            $attributes[$key] = $value;
+          }
+        }
+      }
+      else {
+        $data = $item;
+      }
+      if (count($children) > 0) {
+        // Render nested list.
+        $data .= theme_item_list(array('items' => $children, 'title' => NULL, 'type' => $type, 'attributes' => $attributes));
+      }
+      
+      $output .= '<li' . drupal_attributes($attributes) . '>' . $data . "</li>\n";
+    }
+    $output .= "</$type>";
+  }
+  
+  return $output;
+
+}
 
 
