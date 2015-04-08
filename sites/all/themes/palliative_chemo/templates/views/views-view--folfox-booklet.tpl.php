cdc25d6533cd5cae5bc7a1d0bda8fe0b0b328943
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-04-08 14:57:48 -0400

    Added views template override for pal chemo booklet page skipping, added Devel module

---
+++ sites/all/themes/palliative_chemo/templates/views/views-view--folfox-booklet.tpl.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * @file
+ * Main view template.
+ *
+ * Variables available:
+ * - $classes_array: An array of classes determined in
+ *   template_preprocess_views_view(). Default classes are:
+ *     .view
+ *     .view-[css_name]
+ *     .view-id-[view_name]
+ *     .view-display-id-[display_name]
+ *     .view-dom-id-[dom_id]
+ * - $classes: A string version of $classes_array for use in the class attribute
+ * - $css_name: A css-safe version of the view name.
+ * - $css_class: The user-specified classes names, if any
+ * - $header: The view header
+ * - $footer: The view footer
+ * - $rows: The results of the view query, if any
+ * - $empty: The empty text to display if the view is empty
+ * - $pager: The pager next/prev links to display, if any
+ * - $exposed: Exposed widget form/info to display
+ * - $feed_icon: Feed icon to display, if any
+ * - $more: A link to view more, if any
+ *
+ * @ingroup views_templates
+ */
+?>
+<div class="<?php print $classes; ?>">
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <?php print $title; ?>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <?php if ($header): ?>
+    <div class="view-header">
+      <?php print $header; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($exposed): ?>
+    <div class="view-filters">
+      <?php print $exposed; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($attachment_before): ?>
+    <div class="attachment attachment-before">
+      <?php print $attachment_before; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($rows): ?>
+    <div class="view-content">
+      <?php print $rows; ?>
+    </div>
+  <?php elseif ($empty): ?>
+    <div class="view-empty">
+      <?php print $empty; ?>
+    </div>
+  <?php endif; ?>
+
+<?php 
+/**
+ * This is an addition to give users the choice of skipping ahead
+ * to a different page of the Palliative Chemo booklet
+ *
+ * Dave Rothfarb 
+ * 4-8-15
+ */
+	$view = views_get_current_view();
+	$current_page = $view->query->pager->current_page;
+	$path = current_path() . '?page=';
+	$decision_links = '<div class="center display-table">';
+	$decision_links .= '<a class="btn btn-primary btn-lg btn-space" href="/' . $path . ($current_page + 1) . '">Learn about life expectency</a>';
+	$decision_links .= '<a class="btn btn-primary btn-lg btn-space" href="/' . $path . ($current_page + 3) . '">Skip to FAQs</a>';
+	$decision_links .= '</div>';
+	if ($pager) {
+		if ($current_page == 14) {
+			// Get rid of next link and reassign to $pager variable
+			$pattern = '/<li class="pager-next">[\W\w\S\s]*<\/li>/';
+			$pager = preg_replace($pattern, '', $pager);
+			$pager .= $decision_links;
+		}
+    print $pager;
+	}
+	?>
+
+  <?php if ($attachment_after): ?>
+    <div class="attachment attachment-after">
+      <?php print $attachment_after; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($more): ?>
+    <?php print $more; ?>
+  <?php endif; ?>
+
+  <?php if ($footer): ?>
+    <div class="view-footer">
+      <?php print $footer; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($feed_icon): ?>
+    <div class="feed-icon">
+      <?php print $feed_icon; ?>
+    </div>
+  <?php endif; ?>
+
+</div><?php /* class view */ ?>
